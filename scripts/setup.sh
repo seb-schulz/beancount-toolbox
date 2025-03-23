ea41a8a5687b492317a295ba0bff546ee4c965ca
Sebastian Schulz <16666034+seb-schulz@users.noreply.github.com>
2025-03-23 15:23:49 +0100

    Migrate to devcontainer of sebatec-eu

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,4 +7,6 @@ rootdir=$(realpath "$(dirname "$0")/..")
 python3 -m venv --clear --upgrade-deps  --prompt="$(basename ${rootdir})" "${rootdir}/venv"
 "${rootdir}/venv/bin/python" -m pip install -r requirements.txt
 
+"${rootdir}/venv/bin/python" -m pip install pytest
+
 npm ci || true
