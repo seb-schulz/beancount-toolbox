f803ee05a3a5a67097cd78bbc93ddbff1acc8877
Sebastian Schulz <16666034+seb-schulz@users.noreply.github.com>
2025-12-02 21:19:22 +0100

    Fix importer

--- beancount_toolbox/importers.py
+++ beancount_toolbox/importers.py
@@ -1,17 +1,14 @@
 import csv as pycsv
 import hashlib
 import io
-import os
 import re
-import sys
 import warnings
-from os import path
 
 import yaml
 from beancount import loader
 from beancount.core import compare, data, flags, getters, number
-from beancount.ingest import importer, scripts_utils
-from beancount.ingest.importers import csv
+from beangulp import importer, scripts_utils
+from beangulp.importers import csv
 
 
 class custom_excel(pycsv.excel):
@@ -338,105 +335,6 @@ def dedect_duplicates(new_entries_by_file, existing_entries):
     return r
 
 
-def ingest(importers_list, detect_duplicates_func=None, hooks=None):
-    """Driver function that calls all the ingestion tools.
-
-    Put a call to this function at the end of your importer configuration to
-    make your import script; this should be its main function, like this:
-
-      from beancount.ingest.scripts_utils import ingest
-      my_importers = [ ... ]
-      ingest(my_importers)
-
-    This more explicit way of invoking the ingestion is now the preferred way to
-    invoke the various tools, and replaces calling the bean-identify,
-    bean-extract, bean-file tools with a --config argument. When you call the
-    import script itself (as as program) it will parse the arguments, expecting
-    a subcommand ('identify', 'extract' or 'file') and corresponding
-    subcommand-specific arguments.
-
-    Here you can override some importer values, such as installing a custom
-    duplicate finding hook, and eventually more. Note that this newer invocation
-    method is optional and if it is not present, a call to ingest() is generated
-    implicitly, and it functions as it used to. Future configurable
-    customization of the ingestion process will be implemented by inserting new
-    arguments to this function, this is the motivation behind doing this.
-
-    Note that invocation by the three bean-* ingestion tools is still supported,
-    and calling ingest() explicitly from your import configuration file will not
-    break these tools either, if you invoke them on it; the values you provide
-    to this function will be used by those tools.
-
-    Args:
-      importers_list: A list of importer instances. This is used as a
-        chain-of-responsibility, called on each file.
-      detect_duplicates_func: (DEPRECATED) An optional function which accepts a
-        list of lists of imported entries and a list of entries already existing
-        in the user's ledger. See function find_duplicate_entries(), which is
-        the default implementation for this. Use 'filter_funcs' instead.
-      hooks: An optional list of hook functions to apply to the list of extract
-        (filename, entries) pairs, in order. This replaces
-        'detect_duplicates_func'.
-    """
-    if detect_duplicates_func is not None:
-        warnings.warn("Argument 'detect_duplicates_func' is deprecated.")
-        # Fold it in hooks.
-        if hooks is None:
-            hooks = []
-        hooks.insert(0, detect_duplicates_func)
-        del detect_duplicates_func
-
-    # The script is called directly. This is the main program of the import
-    # script itself. This is the new invocation method.
-    parser = scripts_utils.version.ArgumentParser(
-        description=scripts_utils.DESCRIPTION)
-
-    # Use required on subparsers.
-    # FIXME: Remove this when we require version 3.7 or above.
-    kwargs = {}
-    if sys.version_info >= (3, 7):
-        kwargs['required'] = True
-    subparsers = parser.add_subparsers(dest='command', **kwargs)
-
-    parser.add_argument(
-        '--downloads',
-        '-d',
-        metavar='DIR-OR-FILE',
-        action='append',
-        default=[],
-        help='Filenames or directories to search for files to import')
-
-    for cmdname, module in [
-        ('identify', scripts_utils.identify),
-        ('extract', scripts_utils.extract),
-        ('file', scripts_utils.file),
-    ]:
-        parser_cmd = subparsers.add_parser(cmdname, help=module.DESCRIPTION)
-        parser_cmd.set_defaults(command=module.run)
-        module.add_arguments(parser_cmd)
-
-    args = parser.parse_args()
-
-    if not args.downloads:
-        args.downloads.append(os.getcwd())
-
-    # Implement required ourselves.
-    # FIXME: Remove this when we require version 3.7 or above.
-    if not (sys.version_info >= (3, 7)):
-        if not hasattr(args, 'command'):
-            parser.error("Subcommand is required.")
-
-    abs_downloads = list(map(path.abspath, args.downloads))
-    if isinstance(args.command, str):
-        dict([
-            ('identify', scripts_utils.identify),
-            ('extract', scripts_utils.extract),
-            ('file', scripts_utils.file),
-        ])[args.command].run(args,
-                             parser,
-                             importers_list,
-                             abs_downloads,
-                             hooks=hooks)
-    else:
-        args.command(args, parser, importers_list, abs_downloads, hooks=hooks)
-    return 0
+def ingest(*args, **kwargs):
+    warnings.warn("Argument 'detect_duplicates_func' is deprecated.")
+    return scripts_utils.ingest(*args, **kwargs)
